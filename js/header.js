058253eab673bc208261cfd799a6b27516274ae9
ryan@ryantaylor.net <ryan@ryantaylor.net>
2015-01-26 10:55:41 -0800

    Automated commit at 2015-01-26 18:55:40 UTC by middleman-deploy 1.0.0

---
+++ js/header.js
@@ -0,0 +1,12 @@
+// Basic slider subtitles
+$('#subtitles').bjqs({
+    height: 40,
+    width: 300,
+    responsive: true,
+    animtype: 'slide', // accepts 'fade' or 'slide'
+    animduration: 600, // how fast the animation are
+    animspeed: 3500, // the delay between each slide
+    automatic: true, // automatic
+    showcontrols: false, // show next and prev controls           
+    showmarkers: false // Show individual slide markers
+});
